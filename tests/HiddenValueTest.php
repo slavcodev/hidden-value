18b6e4138b84dae1275fdaded6f11a1c3e06d4dc
Veaceslav Medvedev <slavcopost@gmail.com>
2019-09-13 18:10:51 +0200

    Fix HiddenValueTest

--- tests/HiddenValueTest.php
+++ tests/HiddenValueTest.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of Zee Project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/zee/
+ */
 
 declare(strict_types=1);
 
